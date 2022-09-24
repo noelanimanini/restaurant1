4e3efaa6b80eb7cf4692c9bd15b3bebff4ba4917
noelanimanini <crystalnvillanueva@gmail.com>
2022-09-24 15:05:18 -0700

    Updates

---
+++ precache-manifest.a00cdac6cdae2fd8297dda8bf99925ba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ef6045e6a2c994dbd587",
+    "url": "/restaurant1/static/css/main.004dcc1c.chunk.css"
+  },
+  {
+    "revision": "ef6045e6a2c994dbd587",
+    "url": "/restaurant1/static/js/main.ef6045e6.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/restaurant1/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "06e65f191fb631dd96f0",
+    "url": "/restaurant1/static/js/2.06e65f19.chunk.js"
+  },
+  {
+    "revision": "94a47c120a72e14f8182",
+    "url": "/restaurant1/static/js/runtime~main.94a47c12.js"
+  },
+  {
+    "revision": "2ac1219b6eb842c6d8cea23cbe914f29",
+    "url": "/restaurant1/index.html"
+  }
+];

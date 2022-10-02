bce5715cbade1736f823fc635947a3a8a29910a3
noelanimanini <crystalnvillanueva@gmail.com>
2022-10-02 13:08:38 -0700

    finished the landing page

--- restuarant1/src/Landing/landing.js
+++ restuarant1/src/Landing/landing.js
@@ -3,11 +3,31 @@ import "./Landing.css"
 import logo from '../images/alohakitchenlogo.png'
 
 export default function landing() {
+
+  
   return (
-    <div className='landingParent'>
-      <div className="logoParent">
-        <img className="logoChild" src={logo}/>
+    <div >
+      <div className="NavBar">
+          <div id="NavBarChild">
+            <div>
+              CATERING
+            </div>
+            <div>
+              MENU
+            </div>
+            <div>
+              CONTACT
+            </div>
+          </div>
       </div>
+    <div className='landingParent' id="landingParentOverlay">
+      <div className="logoParent">
+        <img className="logoChild" src={logo} alt="aloha-kitchen-logo"/>
+        <div className="alohaAddress">
+        10408 SILVERDALE WAY NW, SILVERDALE, WA 98383
+        </div>
+      </div>
+    </div>
     </div>
   )
 }
